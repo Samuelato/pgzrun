0f2141036d573372f43c110b97ac16e13efeab2e
Samuelato <edithatoneche@gmail.com>
2024-12-09 18:28:30 +0100

    Update recycle game.py

--- recycle game/recycle game.py
+++ recycle game/recycle game.py
@@ -4,7 +4,7 @@ import random
 WIDHT=600
 HEIGHT=590
 #varibles
-List=["plastic-bag.png","straw.jpg","bottle.jpg","batery.jpg"]
+List=["plastic bag.png","straw.png","bottle.png","batery.png"]
 speedo=7
 startspeedo=10
 totallevels=7
@@ -14,7 +14,7 @@ items=[]
 currentlev=1
 def make_items(extraitem):
     newitem=[]
-    options=["paper bag.jpg"]+ random.choices(List,k=extraitem)
+    options=["paperbag.png"]+ random.choices(List,k=extraitem)
     random.shuffle(options)
     for i in options:
         item=Actor(i)
@@ -22,15 +22,18 @@ def make_items(extraitem):
     gap=WIDHT/(len(newitem)+1)
     for index,item in enumerate(newitem):
         item.y=0
-        item.x=(index+1)* gap
-        animate(item,duration=startspeedo-currentlev,on_finished=game,y=HEIGHT)
+        item.x=(index+1)* gap+20
+        animate(item,duration=startspeedo-currentlev,y=HEIGHT)
     return newitem
 def draw():
     screen.blit("background.jpg",(0,0))
     if gameovere:
-        screen.draw.text("GAMEOVER!!!!!!!!!",center=(50,50),fontsize=60,color="white")
-    for item in items:
-        item.draw()
+        screen.draw.text("GAMEOVER!!!!!!!!!",center=(300,295),fontsize=60,color="white")
+    elif gamecompleten:
+        screen.draw.text("You winnnn!!!",center=(300,295),fontsize=63,color="light blue")
+    else:
+        for item in items:
+          item.draw()
 def update():
     global items,currentlev
     if len (items)==0:
@@ -44,7 +47,7 @@ def on_mouse_down(pos):
     global items,currentlev,gamecompleten
     for i in items:
         if i.collidepoint(pos):
-            if "paper bag" in i.image:
+            if "paperbag" in i.image:
                 currentlev,totallevels 
                 if currentlev==totallevels:
                     gamecompleten=True
