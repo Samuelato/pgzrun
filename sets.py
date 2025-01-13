ece5c20e0195f2e9f8408f15a75ea506816ae77e
Samuelato <edithatoneche@gmail.com>
2025-01-13 18:57:58 +0100

    Add files via upload

---
+++ sets.py
@@ -0,0 +1,14 @@
+# creating set
+set1={1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17}
+print(set1)
+print(len(set1))
+set1.add(9090909999999000)
+print(set1)
+set1.remove(10)
+set1.discard(11)
+print(18 in set1)
+print(set1)
+set2={40,90,56,20,10}
+set3={40,20,0,66,80}
+#union
+print(set2|set3)
