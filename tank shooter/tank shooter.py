0491efa18495cf05d6b397fe9506cd9589ddc9d2
Samuelato <edithatoneche@gmail.com>
2025-01-13 18:28:31 +0100

    Update tank shooter.py

--- tank shooter/tank shooter.py
+++ tank shooter/tank shooter.py
@@ -5,7 +5,9 @@ HEIGHT=600
 # definding colors
 blu=(0,0,255)
 Listb=[]
+sco=0
 #actor
+tankbullet=Actor("tankbullet.png")
 tank=Actor("tank.png")
 bug=Actor("bug.png")
 bug.y=0
@@ -13,16 +15,18 @@ bug.x=random.randint(0,1200)
 tank.pos=600,530
 def draw():
     screen.blit("field.jpg",(0,0))
+    screen.draw.text("score:"+str(sco),(20,20))
     bug.draw()
     tank.draw()
     for i in Listb:
         i.draw()
 def on_key_down(key):
     if key==keys.SPACE:
-        Listb.append(Actor("tank bullet.png"))
+        Listb.append(Actor("tankbullet.png"))
         Listb[-1].x=tank.x
         Listb[-1].y=tank.y-50
 def update():
+    global sco
     pass
     if keyboard.left:
         tank.x-=10
@@ -34,5 +38,12 @@ def update():
     if bug.y==600:
         bug.y=0
         bug.x=random.randint(0,1200)
+    for i in Listb:
+        if i.colliderect(bug):
+            sco+=1
+            bug.y=0
+            bug.x=random.randint(0,1200)
+        
+
 
 pgzrun.go()
