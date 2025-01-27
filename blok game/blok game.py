1ce4ea47d810e32c561687b39c0ec6a169fcdadd
Samuelato <edithatoneche@gmail.com>
2025-01-27 18:27:30 +0100

    Update blok game.py

--- blok game/blok game.py
+++ blok game/blok game.py
@@ -2,16 +2,19 @@ import random
 import itertools
 import pgzrun
 HEIGHT=400
-WIDHT=400
+WIDTH=400
 # setting the block positions
-listy=[(20,20),(380,20),(380,380),(20,380)]
+listy=[(380,20),(380,380),(20,380),(20,20)]
 listie=itertools.cycle(listy)
 block=Actor("block.png")
 block.pos=(20,20)
 def draw():
+    screen.clear()
     block.draw()
-
-
+def bounce():
+    animate(block,"bounce_end",duration=0.5,pos=next((listie)))
+bounce()
+clock.schedule_interval(bounce,2)
 
 
 
