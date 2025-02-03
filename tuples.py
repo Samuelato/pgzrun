4e34081d547a7fcb38f3c4d225d9cc1ffd3853ee
Samuelato <edithatoneche@gmail.com>
2025-02-03 18:56:29 +0100

    Add files via upload

---
+++ tuples.py
@@ -0,0 +1,17 @@
+#creating a tuples
+#indexing
+tup=(4.0,"Samuel",6,False,True,"missstutti")
+print(tup[1])
+print(tup[-4])
+ln=len(tup)
+print(ln)
+#concatenation
+tupl=(6,9,8,4,7,1)
+tuple=(2,3,5,10,9090,1000000)
+tax=tupl+tuple
+print(tax)
+#repeating tuples
+taxia=tupl*100
+print(taxia)
+#membership
+print(1000000 in tuple)
